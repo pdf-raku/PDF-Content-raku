e90adc8b28993fcaae67430eea8dc46e8f7fb246
David Warring <david.warring@gmail.com>
2016-12-31 05:49:04 +1300

    tweaked PDF::Content::Image::PDF

--- lib/PDF/Content/Image/PDF.pm
+++ lib/PDF/Content/Image/PDF.pm
@@ -1,16 +1,15 @@
 use v6;
 # rakudo 2016.11 read invocation: No such symbol 'PDF::Lite'
-use PDF::Content::Image;
+use PDF;
 
 class PDF::Content::Image::PDF {
     method read($fh) {
-        my $class = (require ::('PDF::Lite'));
         $fh.seek(0, SeekFromBeginning);
         my $header = $fh.read(4).decode: 'latin-1';
         my $path = $fh.path;
         die X::PDF::Image::WrongHeader.new( :type<PDF>, :$header, :$path )
             unless $header ~~ "%PDF";
-        my $pdf = $class.open($fh);
+        my $pdf = (require ::('PDF::Lite')).open($fh);
         my $page1 = $pdf.page(1) // die "PDF contains no pages";
         $page1.to-xobject;
     }
