pdf-raku/PDF-Content-raku

commit 553d71dd885a8fb0f5659017c8525e0ad14ac667

Add : and : text-box options

lib/PDF/Content/Font/Enc/Glyphic.rakumod
@@ -55,7 +55,7 @@ method differences is rw {
                     self.use-cid($cid);
                     self.cid-map-glyph($name, $cid);
                 }
-                %!diffs{$cid++} = PDF::COS::Name.COERCE($name);
+                %!diffs{$cid} = PDF::COS::Name.COERCE($name);
             }
             multi sub add-diff($_) is hidden-from-backtrace {
                 die "bad difference entry: .raku";

lib/PDF/Content.rakumod
@@ -473,21 +473,17 @@ say $gfx.Str;
     #| map transformed user coordinates to untransformed (default) coordinates
     use PDF::Content::Matrix :&dot, :&inverse-dot;
     method base-coords(*@coords where .elems %% 2, :$user = True, :$text = !$user --> Array) {
-        (
-            my @ = @coords.map: -> $x is copy, $y is copy {
-                ($x, $y) = $.TextMatrix.&dot($x, $y) if $text;
-                slip($user ?? $.CTM.&dot($x, $y) !! ($x, $y));
-            }
-        )
+        my @ = @coords.map: -> $x is copy, $y is copy {
+            ($x, $y) = $.TextMatrix.&dot($x, $y) if $text;
+            slip($user ?? $.CTM.&dot($x, $y) !! ($x, $y));
+        }
     }
     #| inverse of base-coords
     method user-coords(*@coords where .elems %% 2, :$user = True, :$text = !$user --> Array) {
-        (
-            my @ = @coords.map: -> $x is copy, $y is copy {
-                ($x, $y) = $.CTM.&inverse-dot($x, $y) if $user;
-                slip($text ?? $.TextMatrix.&inverse-dot($x, $y) !! ($x, $y));
-            }
-        )
+        my @ = @coords.map: -> $x is copy, $y is copy {
+            ($x, $y) = $.CTM.&inverse-dot($x, $y) if $user;
+            slip($text ?? $.TextMatrix.&inverse-dot($x, $y) !! ($x, $y));
+        }
     }
     method user-default-coords(|c) is DEPRECATED('base-coords') {
         $.base-coords(|c);

lib/PDF/Content/Ops.rakumod
@@ -977,8 +977,8 @@ y | CurveToFinal | x1 y1 x3 y3 | Append curved segment to path (final point repl
     }
     =para This method is only valid in a path context
 
-    #| process operator quarantined by PDF::Grammar::Content as either an unknown operator
-    #| or having an incorrect argument list
+    #| process operator quarantined by PDF::Grammar::Content / PDF::Native::COS as either an
+    #| unknown operator or having an incorrect argument list
     multi method op(SuspectOp $_) {
         given .value {
             my $op = .key;

lib/PDF/Content/Text/Box.rakumod
@@ -186,6 +186,8 @@ method !layup(@atoms is copy) {
     my $height := $!style.font-size;
     my $font := $!style.font;
     my Bool $shape := $!style.shape;
+    my Str $script := $!style.script;
+    my Str $lang := $!style.lang;
     my Bool $kern = $!style.kern;
     $kern //= True if $shape;
 
@@ -210,8 +212,8 @@ method !layup(@atoms is copy) {
                     $word-pad = 0;
                 }
 
-                if $shape {
-                    given $font.shape($atom, :$kern) {
+                if $shape || $script || $lang {
+                    given $font.shape($atom, :$kern, :$script, :$lang) {
                         $word = .[0];
                         $word-width = .[1];
                     }

lib/PDF/Content/Text/Style.rakumod
@@ -9,6 +9,8 @@ has Numeric $.font-size = 16;
 has Numeric $.leading = 1.1;
 has Bool    $.kern;
 has Bool    $.shape;
+has Str     $.script;
+has Str     $.lang;
 has Numeric $!space-width = 300;
 has $!units-per-EM = 1000;
 has TextDirection $.direction = 'ltr';

commit 6392ac6e811938034878d130934291da35f8bd44

Fix encoding regression
Broken in 553d71dd885a8fb0f5659017c8525e0ad14ac66

lib/PDF/Content/Font/Enc/Glyphic.rakumod
@@ -55,7 +55,7 @@ method differences is rw {
                     self.use-cid($cid);
                     self.cid-map-glyph($name, $cid);
                 }
-                %!diffs{$cid} = PDF::COS::Name.COERCE($name);
+                %!diffs{$cid++} = PDF::COS::Name.COERCE($name);
             }
             multi sub add-diff($_) is hidden-from-backtrace {
                 die "bad difference entry: .raku";